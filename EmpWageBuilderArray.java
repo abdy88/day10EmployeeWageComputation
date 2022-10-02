b9b4b16c5e4a87159ef032da35911ac8aefc841a
aytech11 <aytech0110@gmail.com>
2022-10-02 23:07:22 +0530

    UC11

--- EmpWageBuilderArray.java
+++ EmpWageBuilderArray.java
@@ -1,29 +1,35 @@
 package bl.com.empwage;
+import java.util.*;
 
-public class EmpWageBuilderArray {
+public class EmpWageBuilderArray implements IComputeEmpWage {
 
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
 	public int numOfCompany = 0;
-	public CompanyEmpWage[] companyEmpWageArray;
+	LinkedList<CompanyEmpWage> companyEmpWageList;
+	Map<String,CompanyEmpWage> companyToEmpWageMap;
 
 	public EmpWageBuilderArray() {
 
-		companyEmpWageArray = new CompanyEmpWage[5];
-
+		companyEmpWageList = new LinkedList<>();
+		companyToEmpWageMap = new HashMap<>();
 	}
 
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays,
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays,
 				maxHoursPerMonth);
+		
+		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(company, companyEmpWage);
 
 	}
 
 	public void computeEmpWage() {
-		for (int i = 0; i < numOfCompany; i++) {
-			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-			System.out.println(companyEmpWageArray[i]);
+		for (int i = 0; i < companyEmpWageList.size(); i++) {
+			CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 		}
 
 	}
@@ -59,6 +65,12 @@ public class EmpWageBuilderArray {
 
 		return totalEmpHrs * companyEmpWage.empRatePerHour;
 
+	}
+
+	@Override
+	public int getTotalWage(String company) {
+		// TODO Auto-generated method stub
+		return companyToEmpWageMap.get(company).totalEmpWage;
 	}
 
 }
--- Main.java
+++ Main.java
@@ -5,11 +5,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+		IComputeEmpWage empWageBuilder = new EmpWageBuilderArray();
 		empWageBuilder.addCompanyEmpWage("tata",30,5,10);
 		empWageBuilder.addCompanyEmpWage("mahindra",50,7,20);
 		empWageBuilder.computeEmpWage();
 		
+		System.out.println("Total wage for tata" + empWageBuilder.getTotalWage("tata"));
 		
 	}
 
