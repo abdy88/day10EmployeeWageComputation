3e231d5944ac748d707ef5e2af78896b234550ce
aytech11 <aytech0110@gmail.com>
2022-10-02 14:19:12 +0530

    UC5 Calculate wages for a month

--- EmpWage.java
+++ EmpWage.java
@@ -5,7 +5,53 @@ public class EmpWage {
 	public static void main(String[] args) {
 		EmpWage obj1 = new EmpWage();
 		System.out.println("Welcome to employee wage computation program");
-		obj1.calculateDailyWage();
+		obj1.calculateMonthlyWage();
+
+	}
+
+	void calculateMonthlyWage() {
+
+		int wagePerHour = 20;
+		int workingHrs = 10;
+		int isFulltime = 1;
+		int isParttime = 2;
+		int isAbsent = 0;
+		int dailyEmployeeWage = 0;
+		int totalWage = 0;
+
+		for (int i = 1; i <= 20; i++) {
+
+			int checkAttendance = (int) (Math.random() * 3);
+
+			switch (checkAttendance) {
+
+			case 1: {
+				System.out.println("employee is present fulltime");
+				dailyEmployeeWage = wagePerHour * workingHrs;
+				System.out.println("dailyemployeewage = " + dailyEmployeeWage);
+			}
+				break;
+
+			case 2: {
+				System.out.println("employee is present part time");
+				dailyEmployeeWage = wagePerHour * 8;
+				System.out.println("dailyemployeewage = " + dailyEmployeeWage);
+			}
+				break;
+
+			case 0:
+
+			{
+				System.out.println("employee is absent");
+				break;
+			}
+			}
+
+			totalWage = totalWage + dailyEmployeeWage;
+
+		}
+
+		System.out.println("total wage for the month :" + totalWage);
 
 	}
 
@@ -18,32 +64,30 @@ public class EmpWage {
 		int isAbsent = 0;
 
 		int checkAttendance = (int) (Math.random() * 3);
-		
-		switch(checkAttendance)
-		{
-		
-		case 1 :
-			{System.out.println("employee is present fulltime");
+
+		switch (checkAttendance) {
+
+		case 1: {
+			System.out.println("employee is present fulltime");
 			int dailyEmployeeWage = wagePerHour * workingHrs;
-			System.out.println("dailyemployeewage = " + dailyEmployeeWage);}
+			System.out.println("dailyemployeewage = " + dailyEmployeeWage);
+		}
 			break;
-	
-		case 2 :
-		{
+
+		case 2: {
 			System.out.println("employee is present part time");
 			int dailyEmployeeWage = wagePerHour * 8;
-			System.out.println("dailyemployeewage = " + dailyEmployeeWage);}
+			System.out.println("dailyemployeewage = " + dailyEmployeeWage);
+		}
 			break;
-		
+
 		case 0:
-		
+
 		{
-		 System.out.println("employee is absent");
-		 break;
-			}
+			System.out.println("employee is absent");
+			break;
+		}
 		}
 	}
 
 }
-
-
