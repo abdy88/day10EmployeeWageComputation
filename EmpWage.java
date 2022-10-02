75adb16ba360939913bd726f9937b88cfe59a5e5
aytech11 <aytech0110@gmail.com>
2022-10-02 13:45:31 +0530

    UC2 calculate daily employee wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,17 +3,32 @@ package bl.com.empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-
+		EmpWage obj1 = new EmpWage();
 		System.out.println("Welcome to employee wage computation program");
+		obj1.calculateDailyWage();
+
+	}
+
+	void calculateDailyWage() {
+
+		int wagePerHour = 20;
+		int workingHrs = 8;
+		int isPresent = 1;
+		int isAbsent = 0;
 
 		int checkAttendance = (int) (Math.random() * 2);
 
-		if (checkAttendance == 0) {
+		if (checkAttendance == 1) {
 			System.out.println("employee is present");
+			int dailyEmployeeWage = wagePerHour * workingHrs;
+			System.out.println("dailyemployeewage = " + dailyEmployeeWage);
+		} else {
+			System.out.println("employee is absent");
+
+			int dailyEmployeeWage = 0;
+			System.out.println("dailyemployeewage = " + dailyEmployeeWage);
 		}
-		else{
-			System.out.println("employee is absent");}
-		
+
 	}
 
 }
