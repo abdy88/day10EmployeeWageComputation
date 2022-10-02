e5db59f88e64c13b2cfb7d1ddeb2475be861ea08
aytech11 <aytech0110@gmail.com>
2022-10-02 13:33:33 +0530

    UC1 check attendance

--- EmpWage.java
+++ EmpWage.java
@@ -2,4 +2,18 @@ package bl.com.empwage;
 
 public class EmpWage {
 
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to employee wage computation program");
+
+		int checkAttendance = (int) (Math.random() * 2);
+
+		if (checkAttendance == 0) {
+			System.out.println("employee is present");
+		}
+		else{
+			System.out.println("employee is absent");}
+		
+	}
+
 }
